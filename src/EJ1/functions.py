dfa6d79f03c4425eeb710e49904070c2c7603202
SantiAA <sarribere@itba.edu.ar>
2021-05-29 01:31:19 -0300

    mejorado logistic regresion

---
+++ src/EJ1/functions.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+# Sklear functions
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, roc_curve, confusion_matrix, auc
+# keras functions
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import SGD, Adam
+from keras.metrics import AUC # Area under the curve, default: ROC
+from keras.losses import BinaryCrossentropy
+from keras.callbacks import EarlyStopping, LearningRateScheduler
+import kerastuner as kt
+
+def replace_outliers_zeros(data_frame, outlayers_keys: dict, zero_keys: list, mean_median = False, data_to_replace = None):
+    """
+    Fuction to replace outliers from data_frame
+    and replace them with median or mean value
+    Don't afect the original data frame, returns a copy 
+    with the parameters changed
+    """
+    data = data_frame.copy()
+    if len(zero_keys) != 0:
+        for key in zero_keys:
+            data.loc[data[key] == 0, key] = np.NaN
+            
+    replace = {}
+    if not data_to_replace:
+        for x in data.keys():
+            if mean_median:
+                replace[x] = np.mean(data[x].dropna())
+            else:
+                replace[x] = np.median(data[x].dropna())
+    else:
+        replace = data_to_replace.copy()
+        
+    for key, val in outlayers_keys.items():
+        data.loc[data[key] < val[0], key] = np.NaN
+        data.loc[data[key] > val[1], key] = np.NaN
+    
+    for key, val in replace.items():
+        data[key] = data[key].replace(np.NaN, replace[key])
+        
+    return data, replace
+
+
+def normalize(data_frame, data_to_norm=None):
+    data = data_frame.copy()
+    norm_std = {}
+    if not data_to_norm:
+        for key in data:
+            k_mean = np.mean(data[key])
+            k_std = np.std(data[key])
+            norm_std[key] = [k_mean, k_std]
+    else:
+        norm_std = data_to_norm.copy()
+        
+    for key in data:
+        data[key] = (data[key]-norm_std[key][0])/norm_std[key][1]
+        
+    return data, norm_std
+
+def metrics(tn, fp, fn, tp):
+    spec = tn / (tn + fp + 1e-15)
+    neg_pred_val = tn / (tn + fn + 1e-15)
+    sens = tp / (tp + fn + 1e-15)
+    pos_pred_val = tp / (tp + fp + 1e-15)
+    return spec, sens, pos_pred_val, neg_pred_val
+    
+def print_metrics(y, y_pred):
+    fpr, tpr, th = roc_curve(y, y_pred)
+    roc_auc = roc_auc_score(y, y_pred)
+    plt.figure(1)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr, tpr, label='AUC={}'.format(roc_auc))
+    plt.title('ROC Curve')
+    plt.show()
+    tn, fp, fn, tp = confusion_matrix(y, np.rint(y_pred)).ravel()
+    sp, ss, ppv, npv = metrics(tn, fp, fn, tp)
+    
+    
+def verify_model(my_model, x_train, y_train, x_validation, y_validation, train_label='Train', valid_label='Validacion'):
+    y_train_pred = my_model.predict(x_train)
+    y_valid_pred = my_model.predict(x_validation)
+    # Busco la metrica principal
+    roc_auc_val = roc_auc_score(y_validation, y_valid_pred)
+    roc_auc_tra = roc_auc_score(y_train, y_train_pred)
+    # Busco las metricas secundarioas
+    a, b, c, d = confusion_matrix(y_train, np.rint(y_train_pred)).ravel()
+    sp_t, ss_t, ppv_t, npv_t = metrics(a, b, c, d)
+    a, b, c, d = confusion_matrix(y_validation, np.rint(y_valid_pred)).ravel()
+    sp_v, ss_v, ppv_v, npv_v = metrics(a, b, c, d)
+    # Format data
+    d = {
+        'Set': [train_label, valid_label],
+        'AUC ROC': [roc_auc_tra, roc_auc_val],
+        'Especificidad': [sp_t, sp_v],
+        'Sensibilidad': [ss_t, ss_v],
+        'Valor Predictivo Positivo': [ppv_t, ppv_v],
+        'Valor Predictivo Negativo': [npv_t, npv_v]
+    } 
+    return pd.DataFrame(data=d)
